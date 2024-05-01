efdd56f622ffde918e9ce6d1e6083d7a3c0c18bd
harsha-usethread <harsha@usethread.io>
2024-05-01 19:10:09 -0400

    Everything is updateable except for image

--- src/components/Modals/EditProductModal.tsx
+++ src/components/Modals/EditProductModal.tsx
@@ -24,11 +24,24 @@ export const EditProductModal = (props: EditProductModalProps) => {
   const { product, productImage, open, onClose, onUpdate } = props;
   const [productName, setProductName] = useState(product.name);
   const [loadingSave, setLoadingSave] = useState(false);
+  const [productDescription, setProductDescription] = useState(
+    product.description
+  );
+  const [productRegularPrice, setProductRegularPrice] = useState(
+    product.regular_price
+  );
+  const [productSalePrice, setProductSalePrice] = useState(product.sale_price);
   const { loading, user } = useUser();
   const userId = user?.userId || "";
   const handleSave = async () => {
     setLoadingSave(true);
-    const body = { name: productName, userid: userId };
+    const body = {
+      name: productName,
+      description: productDescription,
+      regular_price: productRegularPrice,
+      sale_price: productSalePrice,
+      userid: userId,
+    };
     const response = await patchProduct(product.id, body);
     console.log(response);
     onUpdate();
@@ -61,7 +74,7 @@ export const EditProductModal = (props: EditProductModalProps) => {
           <ModalHeader className="flex flex-col gap-1 text-center">
             Product Information
           </ModalHeader>
-          <ModalBody className="flex flex-col">
+          <ModalBody className="flex flex-col overflow-y-auto">
             <div className="mb-4">
               <strong>Name:</strong>
               <Input
@@ -86,11 +99,30 @@ export const EditProductModal = (props: EditProductModalProps) => {
                 currency: "USD",
               })}
             </div>
-            {product.description && (
-              <div className="mb-4">
-                <strong>Description:</strong> {product.description}
-              </div>
-            )}
+            <div className="mb-4">
+              <strong>Regular Price:</strong>{" "}
+              <Input
+                defaultValue={productRegularPrice}
+                fullWidth
+                onChange={(e) => setProductRegularPrice(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <strong>Sale Price:</strong>{" "}
+              <Input
+                defaultValue={productSalePrice}
+                fullWidth
+                onChange={(e) => setProductSalePrice(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <strong>Description:</strong>
+              <Input
+                defaultValue={productDescription}
+                fullWidth
+                onChange={(e) => setProductDescription(e.target.value)}
+              />
+            </div>
           </ModalBody>
           <ModalFooter>
             <Button
