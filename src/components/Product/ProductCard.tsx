70978bf3fd8ff6729e419a296858621080a77e3f
harsha-usethread <harsha@usethread.io>
2024-05-03 17:50:02 -0400

    Another image fix

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -29,7 +29,7 @@ const ProductCard = (props: ProductCardProps) => {
 
   let productImage = product.images[0];
 
-  if (!isValidUrl(productImage.src)) {
+  if (productImage && !isValidUrl(productImage.src)) {
     productImage = {
       src: "https://t3.ftcdn.net/jpg/04/60/01/36/360_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg",
     };
