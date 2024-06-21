474d5f56054975678d76cd59cc68df8b3211c282
harsha-usethread <harsha@usethread.io>
2024-06-21 15:03:09 -0400

    Changed renders to use chip

--- src/components/Renders.tsx
+++ src/components/Renders.tsx
@@ -2,38 +2,33 @@ import { Chip, ChipProps } from "@nextui-org/react";
 
 export const renderCategories = (product: any) => {
   return (
-    <div className="flex flex-wrap justify-center mt-4">
+    <div className="flex flex-wrap gap-1 w-full">
       {product.categories.map((category: any) => (
-        <span
-          key={category.id}
-          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >
+        <Chip key={category.id} color="default" size="sm">
           {category.name}
-        </span>
+        </Chip>
       ))}
     </div>
   );
 };
 
 export const renderStockStatus = (product: any) => {
-  let statusColor = "bg-green-200 text-green-800";
+  let color: ChipProps["color"] = "success";
   let statusText = "In Stock";
 
   if (
     product.stock_status === "outofstock" ||
     product.stock_status === "onbackorder"
   ) {
-    statusColor = "bg-red-200 text-red-800";
+    color = product.stock_status === "outofstock" ? "danger" : "warning";
     statusText =
       product.stock_status === "outofstock" ? "Out of Stock" : "On Backorder";
   }
 
   return (
-    <div
-      className={`inline-block rounded-full px-3 py-1 text-sm font-semibold ${statusColor}`}
-    >
+    <Chip color={color} variant="solid" size="sm">
       {statusText}
-    </div>
+    </Chip>
   );
 };
 
