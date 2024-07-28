d439ae9894607c6cfb7c89dd6602ed227d5f3b61
harsha-usethread <harsha@usethread.io>
2024-07-28 12:47:39 -0500

    Updating to Subscription Test

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
-  return NextResponse.json({ message: "hello" });
+  return NextResponse.json({ message: "Subscription Test" });
 }
