8b8afc64573e27cdbf24eff7f2b374c96bb9005e
harsha-usethread <harsha@usethread.io>
2024-08-23 10:02:56 -0500

    Added Compose Providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 import { AuthProvider } from "@propelauth/nextjs/client";
 import "@/styles/globals.css";
 import { OrgProvider } from "@/components/context/OrgContext";
@@ -8,20 +8,35 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { NavigationProvider } from "@/components/context/NavigationContext";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  const auth_url = process.env.NEXT_PUBLIC_AUTH_URL as string;
-  const queryclient = new QueryClient();
+const auth_url = process.env.NEXT_PUBLIC_AUTH_URL as string;
+const queryclient = new QueryClient();
+
+// Utility function to compose providers
+const composeProviders =
+  (...providers: React.ComponentType<{ children: ReactNode }>[]) =>
+  ({ children }: { children: ReactNode }): React.ReactElement =>
+    providers.reduceRight(
+      (acc, Provider) => <Provider>{acc}</Provider>,
+      <>{children}</>
+    );
+
+const ComposedProviders = composeProviders(
+  NavigationProvider,
+  OrgProvider
+  // MealsProvider
+  // Add more providers here as needed
+);
+
+export default function Providers({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <AuthProvider authUrl={auth_url}>
         <QueryClientProvider client={queryclient}>
-          <OrgProvider>
-            <NavigationProvider>
-              <NextThemesProvider attribute="class" defaultTheme="dark">
-                <body>{children}</body>
-              </NextThemesProvider>
-            </NavigationProvider>
-          </OrgProvider>
+          <NextThemesProvider attribute="class" defaultTheme="dark">
+            <ComposedProviders>
+              <body>{children}</body>
+            </ComposedProviders>
+          </NextThemesProvider>
         </QueryClientProvider>
       </AuthProvider>
     </html>
