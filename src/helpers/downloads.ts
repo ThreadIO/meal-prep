2bac7d1d4fa294af4b5f844684a1bce65d402a8f
harsha-usethread <harsha@usethread.io>
2024-08-24 11:05:55 -0500

    Adding Meal Options to the label

--- src/helpers/downloads.ts
+++ src/helpers/downloads.ts
@@ -1,92 +1,107 @@
 import { getDeliveryDate } from "@/helpers/date";
+import { decodeHtmlEntities } from "@/helpers/frontend";
 
 export const generateFullCsvData = (orders: any[], meals: any = []) => {
-  // This means that the meal doesn't exist (stopgap solution for now before meals are updated in the database)
+  const extractAllergens = (acf: any) => {
+    const allergens = acf?.allergens?.items
+      ?.map((item: any) => item.label.name)
+      .join(", ");
+    return allergens || "";
+  };
+
+  const formatSize = (metaData: any[]) => {
+    const addonsData = metaData.find((item) => item.key === "_addons_data");
+    if (addonsData) {
+      const sizeAddons = addonsData.value.filter(
+        (addon: any) => addon.name === "Size"
+      );
+      const sizes = sizeAddons.map((addon: any) => addon.value);
+      return sizes.join(" | ");
+    }
+    return "";
+  };
+
+  const calculateFacts = (selectedSize: string, metaData: any[]) => {
+    if (!metaData) {
+      return null;
+    }
+    const sizeData = metaData.find((item) => item.name === "Size");
+    if (sizeData) {
+      const selectedOption = sizeData.options.find(
+        (option: any) => option.label === selectedSize
+      );
+      if (selectedOption) {
+        const calories = parseInt(selectedOption.calories) || 0;
+        const protein = parseInt(selectedOption.protein) || 0;
+        const carbs = parseInt(selectedOption.carbs) || 0;
+        const fat = parseInt(selectedOption.fat) || 0;
+        return {
+          calories: calories,
+          protein: protein,
+          carbs: carbs,
+          fat: fat,
+        };
+      }
+    }
+    return {
+      calories: 0,
+      protein: 0,
+      carbs: 0,
+      fat: 0,
+    };
+  };
+
+  const extractMealOption = (item: any) => {
+    if (item.composite_parent) {
+      const compositeData = item.meta_data.find(
+        (meta: any) => meta.key === "_composite_data"
+      );
+      if (compositeData && compositeData.value) {
+        const component: any = Object.values(compositeData.value).find(
+          (comp: any) => comp.product_id === item.product_id
+        );
+        return component ? component.title : "";
+      }
+    }
+    return "";
+  };
+
+  const csvHeader = [
+    "Customer Name",
+    "First Name",
+    "Last Name",
+    "Product Name",
+    "Expiry Date",
+    "Calories",
+    "Protein",
+    "Carbs",
+    "Fat",
+    "Ingredients",
+    "Allergens",
+    "Delivery Date",
+    "All Sizes",
+    "Meal Option",
+  ];
+
+  const csvData = [csvHeader];
+
+  console.log("Generating CSV data for: ", orders);
+
   if (!meals || meals.length === 0) {
-    const extractAllergens = (acf: any) => {
-      const allergens = acf?.allergens?.items
-        ?.map((item: any) => item.label.name)
-        .join(", ");
-      return allergens || "";
-    };
-
-    const formatSize = (metaData: any[]) => {
-      const addonsData = metaData.find((item) => item.key === "_addons_data");
-      if (addonsData) {
-        const sizeAddons = addonsData.value.filter(
-          (addon: any) => addon.name === "Size"
-        );
-        const sizes = sizeAddons.map((addon: any) => addon.value);
-        return sizes.join(" | ");
-      }
-      return ""; // Return empty string if size is not found or metadata doesn't contain addons data
-    };
-
-    const calculateFacts = (selectedSize: string, metaData: any[]) => {
-      if (!metaData) {
-        return null;
-      }
-      const sizeData = metaData.find((item) => item.name === "Size");
-      if (sizeData) {
-        const selectedOption = sizeData.options.find(
-          (option: any) => option.label === selectedSize
-        );
-        if (selectedOption) {
-          const calories = parseInt(selectedOption.calories) || 0;
-          const protein = parseInt(selectedOption.protein) || 0;
-          const carbs = parseInt(selectedOption.carbs) || 0;
-          const fat = parseInt(selectedOption.fat) || 0;
-          return {
-            calories: calories,
-            protein: protein,
-            carbs: carbs,
-            fat: fat,
-          };
-        }
-      }
-      return {
-        calories: 0,
-        protein: 0,
-        carbs: 0,
-        fat: 0,
-      };
-    };
-
-    const csvData = [
-      [
-        "Customer Name",
-        "First Name",
-        "Last Name",
-        "Product Name",
-        "Expiry Date",
-        "Calories",
-        "Protein",
-        "Carbs",
-        "Fat",
-        "Ingredients",
-        "Allergens",
-        "Delivery Date",
-        "All Sizes",
-      ],
-    ];
-
-    console.log("Generating CSV data for: ", orders);
     orders.forEach((order) => {
       const deliveryDate = getDeliveryDate(order);
       order.line_items.forEach((item: any) => {
-        // Generate a label for each instance of the line item
         for (let i = 0; i < item.quantity; i++) {
           const allergens = extractAllergens(item.product_data.acf);
-          const selectedSizeRegex = /(\d+\s*cal(?:ories)?)/i; // Match "400cal" or "400 Calories", case insensitive
+          const selectedSizeRegex = /(\d+\s*cal(?:ories)?)/i;
           const selectedSizeMatch = selectedSizeRegex.exec(
             formatSize(item.meta_data)
           );
           let selectedSize = "";
           if (selectedSizeMatch) {
-            selectedSize = selectedSizeMatch[1]; // Use the matched value
+            selectedSize = selectedSizeMatch[1];
           } else {
             const sizeOptions = formatSize(item.meta_data).split(" | ");
-            // Prioritize "400cal" if it exists
             selectedSize =
               sizeOptions.find((size: any) => size.includes("cal")) || "";
           }
@@ -94,33 +109,34 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
             selectedSize,
             item.product_data.product_addons
           );
+          const mealOption = extractMealOption(item);
           csvData.push([
             `${order.billing.first_name} ${order.billing.last_name}`,
             order.billing.first_name,
             order.billing.last_name,
-            `"${item.name}"`, // Enclose item name in double quotes
+            `"${decodeHtmlEntities(item.name)}"`,
             deliveryDate
               ? new Date(deliveryDate.getTime() + 4 * 24 * 60 * 60 * 1000)
                   .toISOString()
                   .slice(0, 10)
-              : "", // Add 4 days if deliveryDate is not null
+              : "",
             (parseInt(item.product_data.acf?.facts?.calories) || 0) +
-              (facts ? facts.calories : 0), // Add existing calories to calculated calories, with null check for 'facts'
+              (facts ? facts.calories : 0),
             (parseInt(
               item.product_data.acf?.facts?.items?.find(
                 (fact: any) => fact.label === "protein"
               )?.amount
-            ) || 0) + (facts ? facts.protein : 0), // Add existing protein to calculated protein, with null check for 'facts'
+            ) || 0) + (facts ? facts.protein : 0),
             (parseInt(
               item.product_data.acf?.facts?.items?.find(
                 (fact: any) => fact.label === "carbs"
               )?.amount
-            ) || 0) + (facts ? facts.carbs : 0), // Add existing carbs to calculated carbs, with null check for 'facts'
+            ) || 0) + (facts ? facts.carbs : 0),
             (parseInt(
               item.product_data.acf?.facts?.items?.find(
                 (fact: any) => fact.label === "fat"
               )?.amount
-            ) || 0) + (facts ? facts.fat : 0), // Add existing fat to calculated fat, with null check for 'facts'
+            ) || 0) + (facts ? facts.fat : 0),
             `"${
               item.product_data.acf?.ingredients?.description?.replace(
                 /<[^>]+>/g,
@@ -128,48 +144,28 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
               ) || ""
             }"`,
             allergens,
-            deliveryDate ? deliveryDate.toISOString().slice(0, 10) : "", // Convert deliveryDate to ISO string if not null
+            deliveryDate ? deliveryDate.toISOString().slice(0, 10) : "",
             formatSize(item.meta_data),
+            mealOption,
           ]);
         }
       });
     });
-
-    return csvData.map((row) => row.join(",")).join("\n");
   } else {
-    const csvData = [
-      [
-        "Customer Name",
-        "First Name",
-        "Last Name",
-        "Product Name",
-        "Expiry Date",
-        "Calories",
-        "Protein",
-        "Carbs",
-        "Fat",
-        "Ingredients",
-        "Allergens",
-        "Delivery Date",
-        "All Sizes",
-      ],
-    ];
-
-    console.log("Generating CSV data for: ", orders);
     orders.forEach((order: any) => {
       const deliveryDate = getDeliveryDate(order);
       order.line_items.forEach((item: any) => {
         for (let i = 0; i < item.quantity; i++) {
-          // Filter all metadata entries that contain "size" and do not start with '_'
-          const sizeMetadata = item.meta_data.filter(
-            (meta: any) => !meta.key.startsWith("_")
-          );
-
-          // Concatenate all sizes with a delimiter '|'
           const selectedSizes =
-            sizeMetadata.map((meta: any) => meta.value).join(" | ") || "";
+            item.meta_data
+              .filter((meta: any) => !meta.key.startsWith("_"))
+              .map((meta: any) => meta.value)
+              .join(" | ") || "";
 
-          const meal = meals.find((m: any) => m.name === item.name);
+          const meal = meals.find(
+            (m: any) =>
+              decodeHtmlEntities(m.name) === decodeHtmlEntities(item.name)
+          );
 
           let calories = 0,
             protein = 0,
@@ -179,17 +175,14 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
             allergens = "",
             allSizes = "";
 
-          // This might need to get fixed in the future
           if (meal) {
             let selectedOption;
             let customOption;
 
-            // Check regular options first
             selectedOption = meal.options.find((opt: any) =>
               selectedSizes.includes(opt.name)
             );
 
-            // If no regular option is found, check custom options
             if (!selectedOption) {
               for (const customOptionGroup of meal.custom_options) {
                 customOption = customOptionGroup.options.find((opt: any) =>
@@ -220,7 +213,6 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
                 )
                 .join(" | ");
             } else {
-              // If no option is found, use the base nutrition facts
               calories = meal.nutrition_facts.calories || 0;
               protein = meal.nutrition_facts.protein || 0;
               carbs = meal.nutrition_facts.carbs || 0;
@@ -229,17 +221,15 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
               allergens = meal.allergens || "";
               allSizes = "";
             }
-          } else {
-            // If no meal is found, set all values to default
-            calories = protein = carbs = fat = 0;
-            ingredients = allergens = allSizes = "";
           }
+
+          const mealOption = extractMealOption(item);
 
           csvData.push([
             `${order.billing.first_name} ${order.billing.last_name}`,
             order.billing.first_name,
             order.billing.last_name,
-            `"${item.name}"`,
+            `"${decodeHtmlEntities(item.name)}"`,
             deliveryDate
               ? new Date(deliveryDate.getTime() + 4 * 24 * 60 * 60 * 1000)
                   .toISOString()
@@ -253,11 +243,12 @@ export const generateFullCsvData = (orders: any[], meals: any = []) => {
             `"${allergens}"`,
             deliveryDate ? deliveryDate.toISOString().slice(0, 10) : "",
             selectedSizes || allSizes,
+            mealOption,
           ]);
         }
       });
     });
-
-    return csvData.map((row) => row.join(",")).join("\n");
   }
+
+  return csvData.map((row) => row.join(",")).join("\n");
 };
